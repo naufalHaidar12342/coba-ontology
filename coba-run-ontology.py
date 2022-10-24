02bb8b6f904fe4b83238e80b179ee09a431faedf
naufalHaidar12342 <111201912342@mhs.dinus.ac.id>
2022-10-24 13:27:28 +0700

    berhasil melihat isi dari file .owl

--- coba-run-ontology.py
+++ coba-run-ontology.py
@@ -4,7 +4,7 @@ from owlready2 import *
 ontology_file=get_ontology("CKB.owl").load(reload=True)
 
 print(ontology_file.classes())
-list_of_classes=list(ontology_file.classes())
+list_of_classes=list(ontology_file.individuals())
 print(list_of_classes)
 print(ontology_file.individuals())
 print(ontology_file.object_properties())
